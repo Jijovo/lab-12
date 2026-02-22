9e91ff68ca5c6bc1125a3df0a3130f03ad65eab5
Jijovo <jijovo77@gmail.com>
2026-02-22 11:37:35 -0800

    update 3

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 // Ezzat Mohamadein | ComSC 210 | Lab12
 using namespace std;
 #include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <array>
 
 int main() {
     //creates an array that stores the model year of the available cars at a dealership
     const int size = 30;
-    int carYears[size];
+    array<int, size> carYears;
     ifstream fin ("input.txt");
     //reads the model year of each car from the file and store it in the array.
     for (int i = 0; i < 30; i++) {
@@ -26,7 +29,7 @@ int main() {
         cout << "Exiting Index Finder." << endl;
     }
     else {
-        cout << "Invalid index. << endl";
+        cout << "Invalid index." << endl;
     }
     //returns the average model year of all the cars in the dealership
     int sum = 0;
@@ -36,8 +39,20 @@ int main() {
     int average = sum / size;
     cout << "The average model year of all the cars in the dealership is: " << average << endl;
     //returns the oldest and newest model year of the cars in the dealership
-    cout << "The oldest model year of the cars in the dealership is: " << *min_element(carYears, carYears + size) << endl;
-    cout << "The newest model year of the cars in the dealership is: " << *max_element(carYears, carYears + size) << endl;
+    cout << "The oldest model year of the cars in the dealership is: " << *min_element(carYears.begin(), carYears.end()) << endl;
+    cout << "The newest model year of the cars in the dealership is: " << *max_element(carYears.begin(), carYears.end()) << endl;
+    //prints current array, sorts it in ascending order, and prints the sorted array.
+    cout << "Current array: ";
+    for (int i = 0; i < size; i++) {
+        cout << carYears[i] << " ";
+    }
+    cout << endl << "Sorting array..." << endl;
+    sort(carYears.begin(), carYears.end());
+    cout << "Sorted array: ";
+    for (int i = 0; i < size; i++) {
+        cout << carYears[i] << " ";
+    }
+    cout << endl;
 
     return 0;
 }
